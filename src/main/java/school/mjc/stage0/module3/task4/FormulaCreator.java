77cf80248f41997ec3ab7885edf75621c5e07ba9
andsps <spirkevichusandrei@mail.ru>
2024-01-12 13:13:31 +0300

    Some Solutions

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -4,7 +4,7 @@ public class FormulaCreator {
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
-        int result = ((9 * a) * 2 - 5 * b + 2 + a - 7) * (a + b - (4 * a * b) / 2);
+        int result = (9 * (a * 2) - 5 * b + 2 + a - 7) * (a + b - (4 * a * b) / 2);
         System.out.println(result);
     }
 }
