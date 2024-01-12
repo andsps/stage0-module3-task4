4f909eab8d2e0c2952f857028528ab188c9c9d32
andsps <spirkevichusandrei@mail.ru>
2024-01-12 14:25:58 +0300

    Some Solutions

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,9 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int result = (9 * (a * 2) - 5 * b + 2 + a - 7) * (a + b - (4 * a * b) / 2);
+        double a = 1;
+        double b = 2;
+        double result = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * (a + b - 4 * a * b / 2);
         System.out.println(result);
     }
 }
